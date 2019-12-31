3ffd2fdf3e7b67a441aafb78eb16b5b912155909
kreotib <kreotib@gmail.com>
2019-12-31 10:15:48 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -101,4 +101,10 @@ $(document).ready(function(){
         })
       })
       })(jQuery)
+      $("a.main-nav__link").click(function () {
+        var elementClick = $(this).attr("href")
+        var destination = $(elementClick).offset().top;
+        jQuery("html:not(:animated),body:not(:animated)").animate({scrollTop: destination}, 800);
+        return false;
+      });
     });
