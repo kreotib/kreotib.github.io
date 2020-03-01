9023f69b0c3e4e28e5e219e4a986332214b1f971
kreotib <kreotib@gmail.com>
2020-03-01 22:56:32 +0300

    qqq

--- js/main.js
+++ js/main.js
@@ -1,199 +1,143 @@
 // Импортируем другие js-файлы
+
 $(document).ready(function() {
-  //NAV SCROOLL
-  var $page = $("html, body");
-  $('.header a[href*="#"]').click(function() {
-    let href = $($.attr(this, "href")).offset().top;
-    let headerHeight = $(".header").height();
-    $page.animate(
-      {
-        scrollTop: href - headerHeight
-      },
-      1500
-    );
-    return false;
-  });
-  $('.footer-list__item a[href*="#"]').click(function() {
-    let href = $($.attr(this, "href")).offset().top;
-    let headerHeight = $(".header").height();
-    $page.animate(
-      {
-        scrollTop: href - headerHeight
-      },
-      1500
-    );
-    return false;
-  });
-  $(".slider-test").slick({
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    fade: true,
-    cssEase: "linear",
-    nextArrow: '<button class="slick-next"></button>',
-    prevArrow: '<button class="slick-prev"></button>',
-    dots: true,
-    customPaging: function(slider, i) {
-      return '<div class="pager__item" id=' + i + ">  </div>";
+  new WOW().init();
+  const levelLink = $(".level-link"),
+    levelContent = $(".level-content"),
+    levelList = $(".level-list"),
+    body = $("body"),
+    toggleLink = $(".toggle-link"),
+    toggleBlock = $(".toggle-block"),
+    reviewVideoWrapper = $(".video-review__wrapper"),
+    reviewControlButton = $(".video-review__button"),
+    reviewList = $(".reviews__list"),
+    mobileMenu = $(".mobile-menu"),
+    modalBlockButtonOpen = $(".modal-block__open"),
+    modalBlockButtonClose = $(".modal-block__close"),
+    modalBlock = $(".modal-block"),
+    reviewListItem = $(".reviews__item"),
+    reviewMore = $(".more_link a");
+
+  //REVIEW ITEM
+  reviewListItem.map(function(index, element) {
+    $(element).addClass("hide");
+    if (index <= 2) {
+      $(element).removeClass("hide");
     }
   });
-  $(".certificates__slider").slick({
-    slidesToShow: 4,
-    slidesToScroll: 1,
-    arrows: false,
-    nextArrow: '<button class="slick-next"></button>',
-    prevArrow: '<button class="slick-prev"></button>',
-    responsive: [
-      {
-        breakpoint: 769,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 1,
-          infinite: true
-        }
-      },
-      {
-        breakpoint: 475,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 1
-        }
-      },
-      {
-        breakpoint: 376,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 1,
-          infinite: true
-        }
-      }
-    ]
-  });
-  //SLIDERS SETTINGS
-
-  $(".professors-slider").slick({
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    arrows: true,
-    nextArrow: '<button class="slick-next"></button>',
-    prevArrow: '<button class="slick-prev"></button>',
-    responsive: [
-      {
-        breakpoint: 769,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 1,
-          infinite: true
-        }
-      },
-      {
-        breakpoint: 600,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1
-        }
-      }
-    ]
-  });
-  $(".slider-blog").slick({
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    arrows: true,
-    nextArrow: '<button class="slick-next"></button>',
-    prevArrow: '<button class="slick-prev"></button>',
-    responsive: [
-      {
-        breakpoint: 1200,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 1,
-          infinite: true
-        }
-      },
-      {
-        breakpoint: 600,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1
-        }
-      }
-    ]
-  });
-  //SLIDER BUTTON SETTINGS
-  $(".slider-one").slick({
-    arrows: true,
-    nextArrow: '<button class="slick-next"></button>',
-    prevArrow: '<button class="slick-prev"></button>'
-  });
-  $(".forWhom-slider").slick({
-    slidesToShow: 2,
-    slidesToScroll: 1,
-    arrows: true,
-    nextArrow: '<button class="slick-next"></button>',
-    prevArrow: '<button class="slick-prev"></button>',
-    responsive: [
-      {
-        breakpoint: 769,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-          infinite: true
-        }
-      }
-    ]
-  });
-
-  //MOBILE MENU SETTINGS
-
-  const showButton = $(".menu-show"),
-    hideButton = $(".menu-hide"),
-    menuMobile = $(".main-nav.mobile");
-
-  $(showButton).click(() => {
-    $(menuMobile).addClass("active");
-    $("body").addClass("overflow");
-  });
-  $(hideButton).click(() => {
-    $(menuMobile).removeClass("active");
-    $("body").removeClass("overflow");
-  });
-
-  $('.mobile-nav a[href*="#"]').click(function() {
-    let href = $($.attr(this, "href")).offset().top;
-    let headerHeight = $(".header").height();
-    $("html, body").animate(
-      {
-        scrollTop: href - headerHeight
-      },
-      1500
-    );
-    return false;
-  });
-  $(".mobile-nav a").click(function() {
-    $(".main-nav.mobile").removeClass("active");
-    $("body").removeClass("overflow");
-  });
-  ///PHOTO CONTAINER
-  let photoContainer = $(".photo-container");
-  $(".certificates__item").click(function() {
-    let certificerImg = $(this).find("img");
-    $(photoContainer)
-      .empty()
-      .addClass("active");
-    $("body").addClass("overflow");
-    $(certificerImg)
-      .clone()
-      .appendTo(".photo-container");
-  });
-  $(".photo-container").click(function(e) {
-    let photoContainerImg = $(".photo-container img");
-    if (e.target != photoContainer) {
-      $("body").removeClass("overflow");
-      $(photoContainer).removeClass("active");
+  $(reviewMore).click(function(e) {
+    e.preventDefault();
+    let reviewItem = document.querySelector(".reviews__item.hide");
+    $(reviewItem).removeClass("hide").addClass('slideInUp');
+    if (!reviewItem) {
+      $(".more_link").hide();
     }
   });
-  //INSTITUE SLICK NEXT
-  let dotsLeft = $(".slider-test .slick-dots").offset().left;
-  let dotsWidth = $(".slider-test .slick-dots").width();
-  let dotsSum = +dotsLeft + dotsWidth + 15;
-  $(".slider-test .slick-next").css("left", dotsSum);
+  //MODAL BLOCK SETTING
+  $(modalBlockButtonOpen).click(function() {
+    $(modalBlock).addClass("open");
+    $(body).addClass("overflow");
+  });
+  $(modalBlockButtonClose).click(function() {
+    $(modalBlock).removeClass("open");
+    $(body).removeClass("overflow");
+  });
+  //MOBILE MENU SETTING
+  $(mobileMenu).click(function() {
+    $(this).toggleClass("active");
+  });
+  //Level show/hide settings
+  $(levelLink).click(function() {
+    $(levelContent).addClass("active");
+    $(body).addClass("overflow");
+  });
+  $(".level-content").mouseup(function(e) {
+    if (levelList.has(e.target).length === 0) {
+      levelContent.removeClass("active");
+      body.removeClass("overflow");
+    }
+  });
+
+  //Toggle block settings
+  $(toggleLink).click(function(e) {
+    e.preventDefault();
+    $(this)
+      .parent()
+      .find(toggleBlock)
+      .toggle(500);
+  });
+
+  //Review block settings
+  $(reviewControlButton).click(function(e) {
+    e.preventDefault();
+    if ($(reviewVideoWrapper).hasClass("active")) {
+      $(reviewList).removeClass("hide");
+      $(reviewVideoWrapper).removeClass("active");
+      $(this)
+        .addClass("video")
+        .text("'Живой отзыв'");
+    } else {
+      $(reviewList).addClass("hide");
+      $(reviewVideoWrapper).addClass("active");
+      $(this)
+        .removeClass("video")
+        .text("Отзывы");
+    }
+  });
+  let offerSectionTop = $("#offer-section").offset().top;
+  $(document).scroll(function() {
+    if ($(document).scrollTop() > offerSectionTop) {
+      $(".contact-form").addClass("active");
+    }
+  });
+});
+
+//SCROLL TO ANCHORN
+$(document).on("click", 'a[href^="#"]', function(event) {
+  event.preventDefault();
+
+  $("html, body").animate(
+    {
+      scrollTop: $($.attr(this, "href")).offset().top
+    },
+    1500
+  );
+});
+
+
+//INPUT PHONE MASK NATIVE JS
+window.addEventListener("DOMContentLoaded", function() {
+  function setCursorPosition(pos, elem) {
+    elem.focus();
+    if (elem.setSelectionRange) elem.setSelectionRange(pos, pos);
+    else if (elem.createTextRange) {
+      var range = elem.createTextRange();
+      range.collapse(true);
+      range.moveEnd("character", pos);
+      range.moveStart("character", pos);
+      range.select();
+    }
+  }
+
+  function mask(event) {
+    var matrix = "+38 (___) ___ __ __",
+      i = 0,
+      def = matrix.replace(/\D/g, ""),
+      val = this.value.replace(/\D/g, "");
+    if (def.length >= val.length) val = def;
+    this.value = matrix.replace(/./g, function(a) {
+      return /[_\d]/.test(a) && i < val.length
+        ? val.charAt(i++)
+        : i >= val.length
+        ? ""
+        : a;
+    });
+    if (event.type == "blur") {
+      if (this.value.length == 2) this.value = "";
+    } else setCursorPosition(this.value.length, this);
+  }
+  var input = document.querySelector(".input-phone");
+  input.addEventListener("input", mask, false);
+  input.addEventListener("focus", mask, false);
+  input.addEventListener("blur", mask, false);
 });
