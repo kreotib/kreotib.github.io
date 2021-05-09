837658ebab6edfc2d12b6872fae806786d7333ce
kreotib <kreotib@gmail.com>
2021-05-09 07:01:57 +0300

    Add files via upload

--- js/common.js
+++ js/common.js
@@ -1,9 +1,19 @@
-$(document).ready(function () {
-    $('.burger').click(function () {
-        const position = $('.main-nav').position();
-       $(this).toggleClass('active');
-       $('body').toggleClass('hidden');
-       $('.nav-list').toggleClass('active');
-       $('.nav-list').css({'top': position.top + $('.main-nav').outerHeight()});
-    });
-});
+// $(document).ready(function () {
+// // Mask
+// $("input[name='phone']").mask("+7(999) 999-99-99");
+// // Fancybox
+// $('.js-link-modal').fancybox({
+//  animationEffect : "zoom-in-out",
+//  slideClass : 'modal-close'
+// });
+// // Slider
+// $('.slider').slick({
+//  prevArrow: '<button type="button" data-role="none" class="slick-prev" aria-label="Previous" tabindex="0" role="button"></button>',
+//  nextArrow: '<button type="button" data-role="none" class="slick-next" aria-label="Next" tabindex="0" role="button"></button>',
+//  infinite: true,
+//  speed: 300,
+//  slidesToShow: 2,
+//  slidesToScroll: 1
+// });
+// });
+"use strict";
