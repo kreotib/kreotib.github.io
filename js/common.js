633bc24847b93e662170e132720f949761f20926
kreotib <kreotib@gmail.com>
2021-07-29 18:15:19 +0300

    Add files via upload

--- js/common.js
+++ js/common.js
@@ -1,19 +1,25 @@
-// $(document).ready(function () {
-// // Mask
-// $("input[name='phone']").mask("+7(999) 999-99-99");
-// // Fancybox
-// $('.js-link-modal').fancybox({
-//  animationEffect : "zoom-in-out",
-//  slideClass : 'modal-close'
-// });
-// // Slider
-// $('.slider').slick({
-//  prevArrow: '<button type="button" data-role="none" class="slick-prev" aria-label="Previous" tabindex="0" role="button"></button>',
-//  nextArrow: '<button type="button" data-role="none" class="slick-next" aria-label="Next" tabindex="0" role="button"></button>',
-//  infinite: true,
-//  speed: 300,
-//  slidesToShow: 2,
-//  slidesToScroll: 1
-// });
-// });
-"use strict";
+const changeTab  = (tab, index = 0)=>{
+    const tabsNavItems = tab.querySelectorAll('.tabs-nav__item'),
+        tabsContentItems = tab.querySelectorAll('.tabs-content__item');
+    index < 0 ? index = 0 : index;
+    index > tabsNavItems.length ? index = tabsNavItems.length - 1 : index;
+    tabsNavItems.forEach((el,ind)=>{
+        ind !== index ? el.classList.remove('active') : el.classList.add('active');
+    });
+    tabsContentItems.forEach((el,ind)=>{
+        ind !== index ? el.classList.remove('active') : el.classList.add('active');
+    });
+}
+$(document).ready(function () {
+    const tabsList = document.querySelectorAll('.tabs');
+    tabsList.forEach(el=>{
+        changeTab(el);
+        const tabsNavLinks = [...el.querySelectorAll('.tabs-nav__link')];
+        tabsNavLinks.forEach((element,index)=>{
+            element.addEventListener('click',(event)=>{
+                event.preventDefault();
+                changeTab(element.closest('.tabs'),index);
+            });
+        });
+    });
+});
