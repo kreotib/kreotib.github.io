bc09d829166a2d4876ef8e060c9d35d55b6fca73
GOD <kreotib@gmail.com>
2021-12-08 14:51:01 +0300

    !

--- js/common.js
+++ js/common.js
@@ -1,2 +1,10 @@
 document.addEventListener('DOMContentLoaded', () => {
+    const phoneCountrySelect = document.querySelectorAll('.phone-country'),
+        phoneCountryArray = [
+            {icon:'img/ukraine.png',number:'+380'},
+            {icon:'img/ukraine.png',number:'+375'},
+            {icon:'img/ukraine.png',number:'+7'},
+            {icon:'img/ukraine.png',number:'+7'},
+            {icon:'img/ukraine.png',number:'+380'},
+        ]
 });
