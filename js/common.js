e36e83519884699b3fc4ba200a918709a525f3be
GOD <kreotib@gmail.com>
2022-01-11 05:38:46 +0300

    !

--- js/common.js
+++ js/common.js
@@ -1,7 +1,30 @@
-const popupShow = (selector) =>{
-    const popup = document.querySelector(selector);
+const popupOpen = (selector) => {
+    const popupBlock = document.querySelector(selector);
 
-    popup.classList.add('active');
+    popupClose();
+
+    popupBlock.classList.add('active');
+};
+
+const popupClose = () => {
+    const popupBlocks = document.querySelectorAll('.popup');
+
+    popupBlocks.forEach(el => {
+        el.classList.remove('active');
+    });
+}
+
+const changeTab = (tabBlock, idx = 0) => {
+    const tabBlockContents = tabBlock.querySelectorAll('.tabs-content__item'),
+        tabBlockLink = tabBlock.querySelectorAll('.tabs-nav__item');
+
+    tabBlockContents.forEach((el, index) => {
+        index !== idx ? el.classList.remove('active') : el.classList.add('active');
+    });
+
+    tabBlockLink.forEach((el, index) => {
+        index !== idx ? el.querySelector('.tabs-nav__link').classList.remove('active') : el.querySelector('.tabs-nav__link').classList.add('active');
+    });
 }
 
 document.addEventListener('DOMContentLoaded', () => {
@@ -23,13 +46,53 @@ document.addEventListener('DOMContentLoaded', () => {
        })
     });
 
-    const popupTriggers = document.querySelectorAll('.popup-trigger');
+    const popupBtns = document.querySelectorAll('.popup-trigger'),
+        popupCloseBtns = document.querySelectorAll('.popup-close'),
+        popupArray = document.querySelectorAll('.popup');
 
-    popupTriggers.forEach(el=>{
-       el.addEventListener('click',(e)=>{
-           e.preventDefault();
+    popupBtns.forEach(el => {
+        el.addEventListener('click', (e) => {
+            e.preventDefault();
+            popupOpen(el.dataset.popup);
+        });
+    })
 
-           popupShow(el.dataset.popup);
-       })
+    popupCloseBtns.forEach(el => {
+        el.addEventListener('click', (e) => {
+            e.preventDefault();
+            popupClose();
+        });
     });
+
+    popupArray.forEach(el => {
+        el.addEventListener('click', (e) => {
+            const elWrapper = el.querySelector('.popup__wrapper');
+            !(elWrapper.contains(e.target)) ? popupClose() : null;
+        });
+    });
+
+    const tabsArr = document.querySelectorAll('.tabs');
+
+    tabsArr.forEach(el=>{
+       changeTab(el,0)
+    });
+
+    document.querySelectorAll('.tabs-nav__link').forEach(el => {
+        el.addEventListener('click', (event) => {
+            event.preventDefault();
+            changeTab(el.closest('.tabs'), [...el.closest('.tabs').querySelectorAll('.tabs-nav__link')].indexOf(event.target))
+        });
+    });
+
+
+    const tableSlider = new Swiper(".table", {
+            direction:"horizontal",
+            slidesPerView: 'auto',
+            freeMode: true,
+            spaceBetween:0,
+            mousewheel: true,
+            scrollbar: {
+                el: ".swiper-scrollbar",
+            },
+        });
 });
