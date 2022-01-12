cf0e843432c64ab473bfc15e5f44ba00da363dfc
GOD <kreotib@gmail.com>
2022-01-12 10:47:15 +0300

    !

--- js/common.js
+++ js/common.js
@@ -110,32 +110,14 @@ document.addEventListener('DOMContentLoaded', () => {
     let gainData = {
         datasets: [{
             data: gain,
-            borderColor: gradient,
             fill:true,
             backgroundColor: gradient,
-            pointRadius: 0,
-            pointBackgroundColor: 'rgba(255, 66, 66, 0.0)',
+            pointBackgroundColor: 'rgba(255, 66, 66, 1)',
+            pointBorderColor:'rgba(256,256,256,1)',
+            pointBorderWidth:3,
+            pointRadius:5,
             order: 0,
-        },{
-            label: 'Line Dataset',
-            type: 'scatter',
-            data: [{
-                x: -10,
-                y: 0
-            }, {
-                x: 0,
-                y: 10
-            }, {
-                x: 10,
-                y: 5
-            }, {
-                x: 0.5,
-                y: 5.5
-            }],
-            backgroundColor: 'rgb(255, 99, 132)',
-            // this dataset is drawn on top
-            order: 1
-        },]
+        }]
     };
     let gainConfig = {
         plugins: {
