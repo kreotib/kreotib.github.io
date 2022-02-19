42c5b75fcbd14c3eab5c633804b656765f8d94f9
kreotib <kreotib@gmail.com>
2022-02-19 13:27:07 +0300

    222

--- academy/js/common.js
+++ academy/js/common.js
@@ -18,6 +18,34 @@ const popupClose = () =>{
     body.classList.remove('no-scroll');
 };
 
+const changeTab = (block, newIndex = 0) => {
+    const tabsNavArray = block.querySelector('.tabs-nav'),
+        tabsContentArray = block.querySelector('.tabs-content'),
+        tabsNavItemArray = tabsNavArray.querySelectorAll('.tabs-nav__item'),
+        tabsContentItemArray = [...tabsContentArray.children];
+
+    changeIndex(tabsNavItemArray, newIndex);
+    changeIndex(tabsContentItemArray, newIndex);
+};
+
+const changeIndex = (array, newIndex) => {
+    array.forEach((el, index) => {
+        index === newIndex ? el.classList.add('active') : el.classList.remove('active')
+    });
+}
+
+const findTabIndex = (el) => {
+    const tabsItemArray = [...el.closest('.tabs').querySelectorAll('.tabs-nav__item')];
+
+    return tabsItemArray.indexOf(el.closest('.tabs-nav__item'));
+}
+
+
+const showHidePassword = (target) =>{
+    const input = target.closest('.form__input').querySelector('input');
+    input.getAttribute('type') === 'password' ? input.setAttribute('type','text') : input.setAttribute('type','password')
+}
+
 document.addEventListener('DOMContentLoaded', () => {
     const popupTrigger = document.querySelectorAll('.popup-trigger'),
         popupArray = document.querySelectorAll('.popup');
@@ -42,5 +70,30 @@ document.addEventListener('DOMContentLoaded', () => {
         el.addEventListener('click',()=>{
            el.closest('.trigger-block').querySelector('.trigger-content').classList.toggle('active');
         });
-    })
+    });
+
+    /*
+    const tabs = document.querySelectorAll('.tabs'),
+        tabsNavLinkArray = document.querySelectorAll('.tabs-nav__link');
+
+    tabs.forEach(el => {
+        changeTab(el);
+    });
+
+    tabsNavLinkArray.forEach(el => {
+        el.addEventListener('click', (e) => {
+            e.preventDefault();
+            changeTab(el.closest('.tabs'), findTabIndex(el));
+        })
+    });*/
+
+    const passwordViewTriggers = document.querySelectorAll('.password-view-trigger');
+
+    passwordViewTriggers.forEach(el=>{
+       el.addEventListener('click',(e)=>{
+           e.preventDefault();
+
+           showHidePassword(el);
+       })
+    });
 });
