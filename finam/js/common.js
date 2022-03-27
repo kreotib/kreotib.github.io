e9d80e5084faba38c6fa01a6b3eabe452584ecaa
kreotib <kreotib@gmail.com>
2022-03-27 22:19:01 +0300

    2

---
+++ finam/js/common.js
@@ -0,0 +1,37 @@
+const changeTab = (block, newIndex = 0) => {
+    const tabsNavArray = block.querySelector('.tabs-nav'),
+        tabsContentArray = block.querySelector('.tabs-content'),
+        tabsNavItemArray = tabsNavArray.querySelectorAll('.tabs-nav__item'),
+        tabsContentItemArray = [...tabsContentArray.children];
+
+    changeIndex(tabsNavItemArray, newIndex);
+    changeIndex(tabsContentItemArray, newIndex);
+};
+const changeIndex = (array, newIndex) => {
+    array.forEach((el, index) => {
+        index === newIndex ? el.classList.add('active') : el.classList.remove('active')
+    });
+}
+const findTabIndex = (el) => {
+    const tabsItemArray = [...el.closest('.tabs').querySelectorAll('.tabs-nav__item')];
+
+    return tabsItemArray.indexOf(el.closest('.tabs-nav__item'));
+}
+
+document.addEventListener('DOMContentLoaded',()=>{
+    const tabs = document.querySelectorAll('.tabs'),
+        tabsNavLinkArray = document.querySelectorAll('.tabs-nav__link');
+
+    if(tabs){
+        tabs.forEach(el => {
+            changeTab(el);
+        });
+
+        tabsNavLinkArray.forEach(el => {
+            el.addEventListener('click', (e) => {
+                e.preventDefault();
+                changeTab(el.closest('.tabs'), findTabIndex(el));
+            })
+        });
+    }
+});
