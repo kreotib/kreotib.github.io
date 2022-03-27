e1e0dc3ef770fcf03c47c292d8a5293172384438
kreotib <kreotib@gmail.com>
2022-03-27 03:37:16 +0300

    barni

--- barni/script/main.js
+++ barni/script/main.js
@@ -15,11 +15,21 @@ const detectBrowserAgent = (browserName) =>{
 }
 
 document.addEventListener('DOMContentLoaded',()=>{
+    const videoBtn = document.querySelector('.video-btn');
+    if(videoBtn){
+        videoBtn.addEventListener('click',(e)=>{
+            e.preventDefault();
+            const video = videoBtn.closest('.video-block').querySelector('.video-item');
+
+            videoBtn.classList.add('hidden');
+            video.play();
+        });
+    }
     const popupTrigger = document.querySelectorAll('.popup-trigger'),
         popupClose = document.querySelectorAll('.popup-close'),
-        heroImgItems = document.querySelectorAll('.hero__img-item'),
-        heroImgItemVideo = document.querySelector('.hero__img-item_video'),
-        heroImgItemImg = document.querySelector('.hero__img-item_img');
+        heroImgItems = document.querySelectorAll('.stock__img-item'),
+        heroImgItemVideo = document.querySelector('.stock__img-item_video'),
+        heroImgItemImg = document.querySelector('.stock__img-item_img');
 
     if(popupTrigger){
         popupTrigger.forEach(el=>{
