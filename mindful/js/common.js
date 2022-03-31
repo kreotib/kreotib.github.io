9fdfe2cd3a83bad91ebd91e6fd9bbc187ff363fe
kreotib <kreotib@gmail.com>
2022-04-01 00:36:04 +0300

    2

--- mindful/js/common.js
+++ mindful/js/common.js
@@ -27,3 +27,18 @@ document.addEventListener('DOMContentLoaded', () => {
         navItem.classList.toggle('active');
     });
 });
+
+    $("a").on('click', function(event) {
+
+        if (this.hash !== "") {
+            event.preventDefault();
+
+            var hash = this.hash;
+
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 800, function(){
+                window.location.hash = hash;
+            });
+        }
+    });
