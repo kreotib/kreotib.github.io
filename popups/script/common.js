b4b9c50ead4a8d1db3049309b43b4ec9746f1dad
kreotib <kreotib@gmail.com>
2022-05-13 16:12:19 +0300

    2

--- popups/script/common.js
+++ popups/script/common.js
@@ -2,24 +2,22 @@ document.addEventListener('DOMContentLoaded', () => {
     const form = document.querySelector('form');
 
     if (form) {
-        form.addEventListener('submit', (e) => {
-            e.preventDefault();
+        const formCheckbox = form.querySelector('.form__checkbox');
+        if(formCheckbox){
+            form.addEventListener('submit', (e) => {
+                e.preventDefault();
 
-            const formCheckbox = form.querySelector('.form__checkbox'),
-                formCheckboxInput = formCheckbox.querySelector('input');
+                const formCheckboxInput = formCheckbox.querySelector('input');
 
-
-            if(formCheckboxInput.checked === true){
-                form.submit()
-            }else{
-                formCheckbox.classList.add('error');
-            }
-        });
-
-        form.addEventListener('click',()=>{
-            const formCheckbox = form.querySelector('.form__checkbox');
-
-            formCheckbox.classList.remove('error');
-        });
+                if(formCheckboxInput.checked === true){
+                    form.submit()
+                }else{
+                    formCheckbox.classList.add('error');
+                }
+            });
+            form.addEventListener('click',()=>{
+                formCheckbox.classList.remove('error');
+            });
+        }
     }
 });
