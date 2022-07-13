e82e59f0c897d98fd08a1c7014b9e49ea8e82df4
kreotib <kreotib@gmail.com>
2022-07-13 19:22:14 +0300

    v-tell

---
+++ v-tell/js/common.js
@@ -0,0 +1,55 @@
+const changeTab = (block, newIndex = 0) => {
+    const tabsNavArray = block.querySelector('.tabs-nav'),
+        tabsContentArray = block.querySelector('.tabs-content'),
+        tabsNavItemArray = tabsNavArray.querySelectorAll('.tabs-nav__item'),
+        tabsContentItemArray = [...tabsContentArray.children];
+
+    changeIndex(tabsNavItemArray, newIndex);
+    changeIndex(tabsContentItemArray, newIndex);
+};
+const changeIndex = (array, newIndex) => {
+    array.forEach((el, index) => {
+        index === newIndex ? el.classList.add('active') : el.classList.remove('active')
+    });
+}
+const findTabIndex = (el) => {
+    const tabsItemArray = [...el.closest('.tabs').querySelectorAll('.tabs-nav__item')];
+
+    return tabsItemArray.indexOf(el.closest('.tabs-nav__item'));
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    const heroSlider = new Swiper('.hero-slider', {
+            loop: true,
+            slidesPerView: 1,
+            pagination: {
+                clickable: true,
+                el: '.hero-slider__pagination',
+            }
+        }),
+        planSlider = new Swiper('.plan-slider', {
+            slidesPerView: 'auto',
+            spaceBetween:39
+        }),
+        navSlider = new Swiper('.plan-tabs__nav', {
+            slidesPerView: 'auto',
+        });
+
+    customSelect('select');
+
+    const tabs = document.querySelectorAll('.tabs'),
+        tabsNavLinkArray = document.querySelectorAll('.tabs-nav__link');
+
+    if(tabs){
+        tabs.forEach(el => {
+            changeTab(el);
+        });
+
+        tabsNavLinkArray.forEach(el => {
+            el.addEventListener('click', (e) => {
+                e.preventDefault();
+                changeTab(el.closest('.tabs'), findTabIndex(el));
+            })
+        });
+    }
+});
