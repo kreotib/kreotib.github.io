f9e39565f2c53ba9918214c9fd51c10e7bc8f0fe
kreotib <kreotib@gmail.com>
2022-07-21 19:10:30 +0300

    2

--- multifreeze/script/script.js
+++ multifreeze/script/script.js
@@ -1,26 +1,39 @@
-document.addEventListener('DOMContentLoaded',()=>{
-   const popupTriggers = document.querySelectorAll('*[data-popup]'),
-       popups = document.querySelectorAll('.popup');
+document.addEventListener('DOMContentLoaded', () => {
+    const popupTriggers = document.querySelectorAll('*[data-popup]'),
+        popups = document.querySelectorAll('.popup'),
+        popupClose = document.querySelectorAll('.popup-close');
 
-   if(popupTriggers.length > 0){
-      popupTriggers.forEach(el=>{
-         el.addEventListener('click',()=>{
-            const popup = document.querySelector(`.${el.dataset.popup}`);
+    if (popupTriggers.length > 0) {
+        popupTriggers.forEach(el => {
+            el.addEventListener('click', (e) => {
+                e.preventDefault();
 
-            document.body.classList.add('hidden');
-            popup.classList.add('active');
-         });
-      });
-   };
+                const popup = document.querySelector(`.${el.dataset.popup}`);
 
-   if(popups.length > 0){
-      popups.forEach(el=>{
-         el.addEventListener('click',(e)=>{
-            if(e.target.classList.contains('popup-wrapper')){
-               document.body.classList.remove('hidden');
-               el.classList.remove('active');
-            }
-         });
-      });
-   }
+                document.body.classList.add('hidden');
+                popup.classList.add('active');
+            });
+        });
+    }
+    ;
+
+    if (popups.length > 0) {
+        popups.forEach(el => {
+            el.addEventListener('click', (e) => {
+                if (e.target.classList.contains('popup-wrapper')) {
+                    document.body.classList.remove('hidden');
+                    el.classList.remove('active');
+                }
+            });
+        });
+    }
+
+    if (popupClose.length > 0) {
+        popups.forEach(el => {
+            el.addEventListener('click', (e) => {
+                document.body.classList.remove('hidden');
+                el.classList.remove('active');
+            });
+        });
+    }
 });
