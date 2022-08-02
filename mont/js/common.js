56c1815cfeed112d38560ffd906c6b6ceeb65b7a
kreotib <kreotib@gmail.com>
2022-08-03 00:44:58 +0300

    2

--- mont/js/common.js
+++ mont/js/common.js
@@ -27,6 +27,34 @@ const expandableListInit = (expandableList, count) => {
     });
 }
 
+const commentTextExpand = (slider) => {
+    const commentItemText = document.querySelectorAll('.comment-item__text');
+
+    commentItemText.forEach(element => {
+        if (element.textContent.length > 220) {
+            const elementFullText = element.textContent,
+                elementSliceText = elementFullText.slice(0, 220);
+
+            element.textContent = elementSliceText;
+
+            const elementLink = document.createElement('a');
+
+            elementLink.setAttribute('href', '#');
+            elementLink.classList.add('comment-item__link');
+            elementLink.textContent = 'читать полностью'
+            element.after(elementLink);
+
+            elementLink.addEventListener('click', (e) => {
+                e.preventDefault();
+
+                elementLink.classList.toggle('active');
+                elementLink.classList.contains('active') ? (elementLink.textContent = 'скрыть', element.textContent = elementFullText) : (elementLink.textContent = 'показать полностью', element.textContent = elementSliceText);
+                slider.updateAutoHeight(500)
+            });
+        }
+    });
+};
+
 document.addEventListener('DOMContentLoaded', () => {
     const triggerLinks = document.querySelectorAll('.trigger-link');
 
@@ -82,10 +110,10 @@ document.addEventListener('DOMContentLoaded', () => {
 
     const cookieAlert = document.querySelector('.cookie-alert');
 
-    if(cookieAlert){
+    if (cookieAlert) {
         const cookieAlertBtn = cookieAlert.querySelector('.cookie-alert-close');
 
-        cookieAlertBtn.addEventListener('click',(e)=>{
+        cookieAlertBtn.addEventListener('click', (e) => {
             e.preventDefault();
 
             cookieAlert.classList.add('hidden');
@@ -94,17 +122,74 @@ document.addEventListener('DOMContentLoaded', () => {
 
     const difficultWrapper = document.querySelector('.difficult-wrapper-input');
 
-    if(difficultWrapper){
+    if (difficultWrapper) {
         const difficultWrapperItem = difficultWrapper.querySelectorAll('.difficult__wrapper-item');
 
-        difficultWrapperItem.forEach((element,index)=>{
-           element.addEventListener('click',()=>{
-               difficultWrapperItem.forEach((newElement, newIndex)=>{
+        difficultWrapperItem.forEach((element, index) => {
+            element.addEventListener('click', () => {
+                difficultWrapperItem.forEach((newElement, newIndex) => {
 
-                  index < newIndex ? newElement.classList.remove('active') : newElement.classList.add('active');
+                    index < newIndex ? newElement.classList.remove('active') : newElement.classList.add('active');
 
-               });
-           });
+                });
+            });
         });
     }
+
+    const starBlock = document.querySelectorAll('.star-block');
+
+    if (starBlock.length > 0) {
+        starBlock.forEach(element => {
+            const starBlockItems = element.querySelectorAll('.star-block-item'),
+                starBlockLinks = element.querySelectorAll('.star-block__link');
+
+            const changeStarBlock = (count) =>{
+                starBlockItems.forEach((starItem, starIndex) => {
+                    starIndex + 1 <= count ? starItem.classList.add('active') : starItem.classList.remove('active');
+                });
+            }
+
+            starBlockLinks.forEach((starLink, starLinkIndex) =>{
+                starLink.addEventListener('click',()=>{
+                   changeStarBlock(starLinkIndex + 1);
+                   element.dataset.count = starLinkIndex + 1;
+                });
+            });
+
+            changeStarBlock(element.dataset.count);
+        });
+    }
+
+    const progress = document.querySelectorAll('.progress');
+
+    if (progress.length > 0) {
+        progress.forEach(element => {
+            const progressItem = element.querySelector('.progress__item'),
+                max = element.dataset.max,
+                current = element.dataset.count,
+                currentPercent = (current / max) * 100;
+
+            progressItem.style.width = `${currentPercent}%`
+        })
+    }
+
+
+    const commentSlider = new Swiper('.comment-slider', {
+        slidesPerView: 'auto',
+        spaceBetween: 40,
+        autoHeight: true,
+        pagination: {
+            el: ".comment-slider-pagination",
+            type: "fraction",
+        },
+        navigation: {
+            nextEl: '.comment-slider-button-next',
+            prevEl: '.comment-slider-button-prev',
+        },
+        on: {
+            beforeInit: function () {
+                commentTextExpand(this);
+            }
+        }
+    });
 });
