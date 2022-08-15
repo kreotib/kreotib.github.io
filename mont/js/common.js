86671e7e0b089aa60c8be18cdd878352b4a0d6b3
kreotib <kreotib@gmail.com>
2022-08-15 17:24:49 +0300

    2

--- mont/js/common.js
+++ mont/js/common.js
@@ -410,6 +410,17 @@ document.addEventListener('DOMContentLoaded', () => {
                 'min': [0],
                 'max': [100]
             },
+            format: {
+                // 'to' the formatted value. Receives a number.
+                to: function (value) {
+                    return value;
+                },
+                // 'from' the formatted value.
+                // Receives a string, should return a number.
+                from: function (value) {
+                    return Number(value.replace(',-', ''));
+                }
+            }
         });
 
         rangeSlider.noUiSlider.on('update', function (values, handle, unencoded, isTap, positions) {
