eb84cf4a39d45ed39d6d94d7741ff93f1dca40ac
kreotib <kreotib@gmail.com>
2022-08-17 19:38:12 +0300

    2

--- m-3/script/script.js
+++ m-3/script/script.js
@@ -22,6 +22,14 @@ const changeSliderTabs = (newIndex = 0) => {
     });
 };
 
+const showPopup = (selector) => {
+
+    const popup = document.querySelector(`.${selector}`);
+
+    document.body.classList.add('hidden');
+    popup.classList.add('active');
+}
+
 document.addEventListener('DOMContentLoaded', () => {
     const sliderTabs = new Swiper(".slider-tabs", {
             slidesPerView: "auto",
@@ -56,14 +64,48 @@ document.addEventListener('DOMContentLoaded', () => {
         })
     });
 
+    const popupTriggers = document.querySelectorAll('*[data-popup]'),
+        popups = document.querySelectorAll('.popup'),
+        popupClose = document.querySelectorAll('.popup-close');
 
-    document.querySelector('.slider-btn').addEventListener('click', (e) => {
-        e.preventDefault();
+    if (popupTriggers.length > 0) {
+        popupTriggers.forEach(el => {
+            el.addEventListener('click', (e) => {
+                e.preventDefault();
+            });
+            el.addEventListener('click',()=>showPopup(el.dataset.popup));
+        });
+    };
 
-        document.querySelector('.section-slider').scrollIntoView({
-            behavior: 'smooth',
-            block: 'start'
-        })
-    });
+    if (popups.length > 0) {
+        popups.forEach(el => {
+            el.addEventListener('click', (e) => {
+                if (e.target.classList.contains('popup-wrapper')) {
+                    document.body.classList.remove('hidden');
+                    el.classList.remove('active');
+                }
+            });
+        });
+    }
 
+    if (popupClose.length > 0) {
+        popups.forEach(el => {
+            el.addEventListener('click', (e) => {
+                document.body.classList.remove('hidden');
+                el.classList.remove('active');
+            });
+        });
+    };
+
+    const pdfButton = document.querySelector('.pdf-mobile');
+
+    if(window.innerWidth < 767){
+        if(popupTriggers.length > 0){
+            popupTriggers.forEach(trigger=>{
+                trigger.addEventListener('click',(e)=>{
+                    window.location.href = trigger.href;
+                });
+            });
+        }
+    }
 });
